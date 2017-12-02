31dd994e6c250e65d2511204960f848a7b69bfd6
Marta Gómez Macías <marta@wazuh.com>
2017-12-02 19:15:21 +0100

    Add Day1 part 2

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,15 +2,30 @@ import sys
 sys.path.append('..')
 from common import read_input_file_integers, str_2_ilist
 from operator import itemgetter
+from itertools import chain
 import argparse
 
 parser = argparse.ArgumentParser(description="Advent of Code 2017: day 1")
-parser.add_argument('-f', '--file', dest='filename', type=str, help="Input problem from file")
-parser.add_argument('-p', '--problem', dest='problem', type=str, help='Input problem from console')
+parser.add_argument('-f', '--file', dest='filename', type=str, help="Input sequence from file")
+parser.add_argument('-s', '--sequence', dest='sequence', type=str, help='Input sequence from console')
+parser.add_argument('-p', '--part', dest='part', type=str, required=True, help="Part of the problem to solve")
 
-def day_1(captcha):
-    # [1,2,3,4] -> [(1,2),(2,3),(3,4),(4,1)]
-    next_element_tuples = zip(captcha,(captcha*2)[1:len(captcha)+1])
+def part_1(captcha):
+    """
+    [1,2,3,4] -> [(1,2),(2,3),(3,4),(4,1)]
+    """
+    return zip(captcha,(captcha*2)[1:len(captcha)+1])
+    
+def part_2(captcha):
+    """
+    [1,2,3,4] -> [(1,3), (2,4), (3,1), (4,2)]
+    """
+    midlen = len(captcha) // 2
+    return chain.from_iterable((zip(captcha[:midlen], captcha[midlen:]),
+                                zip(captcha[midlen:], captcha[:midlen])))
+
+def day_1(captcha, part='1'):
+    next_element_tuples = part_1(captcha) if part == '1' else part_2(captcha)
     # filter the ones that are equal
     equal_elements = filter(lambda x: x[0] == x[1], next_element_tuples)
     # sum the 1st element from each tuple
@@ -21,14 +36,14 @@ if __name__ == '__main__':
     try:
         args = parser.parse_args()
 
-        if args.problem:
-            captcha = str_2_ilist(args.problem)
+        if args.sequence:
+            captcha = str_2_ilist(args.sequence)
         elif args.filename:
             captcha = read_input_file_integers(args.filename)
         else:
             captcha = read_input_file_integers('input')
 
-        print("Solution: {0}".format(day_1(captcha)))
+        print("Solution: {0}".format(day_1(captcha, args.part)))
 
     except Exception as e:
         print("Error in execution: {0}".format(str(e)))
--- tests.py
+++ tests.py
@@ -1,9 +1,17 @@
 from Day1.day1 import day_1
 from common import read_input_file_integers
 
-def test_day1():
-    assert day_1([1,1,2,2]) == 3
-    assert day_1([1,1,1,1]) == 4
-    assert day_1([1,2,3,4]) == 0
-    assert day_1([9,1,2,1,2,1,2,9]) == 9
-    assert day_1(read_input_file_integers('Day1/input')) == 1216
+def test_day1_part1():
+    assert day_1([1,1,2,2], '1') == 3
+    assert day_1([1,1,1,1], '1') == 4
+    assert day_1([1,2,3,4], '1') == 0
+    assert day_1([9,1,2,1,2,1,2,9], '1') == 9
+    assert day_1(read_input_file_integers('Day1/input'), '1') == 1216
+
+def test_day1_part2():
+    assert day_1([1,2,1,2], '2') == 6
+    assert day_1([1,2,2,1], '2') == 0
+    assert day_1([1,2,3,4,2,5], '2') == 4
+    assert day_1([1,2,3,1,2,3], '2') == 12
+    assert day_1([1,2,1,3,1,4,1,5], '2') == 4
+    assert day_1(read_input_file_integers('Day1/input'), '2') == 1072
