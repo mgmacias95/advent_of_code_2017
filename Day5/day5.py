f131ef992581a8e7be1a110c4530adb01385738f
Marta Gómez Macías <marta@wazuh.com>
2017-12-08 00:32:56 +0100

    Starting day 5 problem

---
+++ Day5/day5.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('..')
+import argparse
+from common import basic_arguments, read_input_file_matrix, str_2_imatrix
+
+def iter_jumps(jumps, position=0, n_jumps=0):
+    if position >= len(jumps):
+        return n_jumps
+    else:
+        print("Position: {}. Jumps: {}. Number of jumps: {}".format(position, jumps, n_jumps))
+        jumps[position] += 1
+        iter_jumps(jumps, position+jumps[position]-1, n_jumps+1)
+
+def day_5(jumps, part):
+    return iter_jumps(jumps)
+
+if __name__ == '__main__':
+    try:
+        parser = argparse.ArgumentParser(description="Advent of Code 2017: day 3")
+        basic_arguments(parser)
+        args = parser.parse_args()
+
+        if args.sequence:
+            jumps = str_2_imatrix(args.sequence, plain=True)
+        elif args.filename:
+            jumps = read_input_file_matrix(args.filename, plain=True)
+        else:
+            jumps = read_input_file_matrix('input', plain=True)
+
+        print("Solution: {0}".format(day_5(jumps, args.part)))
+
+    except Exception as e:
+        print("Error in execution: {0}".format(str(e)))
--- common.py
+++ common.py
@@ -1,10 +1,12 @@
+from itertools import chain
+
 def str_2_ilist(str_list):
     """
     '1234' -> [1,2,3,4]
     """
     return list(map(int, str_list))
 
-def str_2_imatrix(str_matrix):
+def str_2_imatrix(str_matrix, plain):
     """
     '1 2 3 4\n1 2 3\n1 2 3 4'
 
@@ -14,7 +16,11 @@ def str_2_imatrix(str_matrix):
     1 2 3
     1 2 3 4
     """
-    return list(map(str_2_ilist, map(lambda x: x.split(' '), str_matrix.split('*'))))
+    matrix = list(map(str_2_ilist, map(lambda x: x.split(' '), str_matrix.split('*'))))
+    if not plain:
+        return matrix
+    else:
+        return list(chain.from_iterable(matrix))
 
 def read_input_file_integers(filepath):
     """
@@ -25,14 +31,14 @@ def read_input_file_integers(filepath):
 
     return str_2_ilist(data.strip())
 
-def read_input_file_matrix(filepath):
+def read_input_file_matrix(filepath, plain=False):
     """
     Read a matrix of integers from file filepath and stores them in a list of lists
     """
     with open(filepath) as f:
         data = f.read()
 
-    return str_2_imatrix(data.replace('\n','*').replace('\t',' ')[:-1]) # remove last \n
+    return str_2_imatrix(data.replace('\n','*').replace('\t',' ')[:-1], plain) # remove last \n
 
 def read_input_file_words(filepath):
     """
